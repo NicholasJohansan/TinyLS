17077c42f38d4f653b50d809af01acf4ff24f1ef
NicholasJohansan <nicholasjohansan@gmail.com>
2022-10-09 23:17:04 +0800

    Compile all html files into index.html

--- src/main.js
+++ src/main.js
@@ -1,14 +1,5 @@
-import $ from "jquery";
-import mainHtml from './html/main.html?raw';
-import titlebarHtml from './html/titlebar.html?raw';
-import modeToggleHtml from './html/modeToggle.html?raw';
-import urlBoxHtml from './html/urlBox.html?raw';
+import $ from 'jquery';
 import.meta.globEager('./styles/*.css'); // import all css files
-
-$('#titlebar').html(titlebarHtml);
-$('#app').html(mainHtml);
-$('#mode-toggle').html(modeToggleHtml);
-$('#url-box').html(urlBoxHtml);
 
 import { bindTitlebarEvents } from './titlebar';
 import { bindModeToggle } from './modeToggle';
