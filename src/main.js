f187fab4b8a578ae546ef2ef2b9f938a48d7e8c5
NicholasJohansan <nicholasjohansan@gmail.com>
2022-10-09 19:07:15 +0800

    Add dark/light mode function

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import $ from "jquery";
 import mainHtml from './html/main.html?raw';
 import titlebarHtml from './html/titlebar.html?raw';
+import modeToggleHtml from './html/modeToggle.html?raw';
 import.meta.globEager('./styles/*.css'); // import all css files
 
 $('#titlebar').html(titlebarHtml);
 $('#app').html(mainHtml);
+$('#mode-toggle').html(modeToggleHtml);
 
 import { bindTitlebarEvents } from './titlebar';
+import { bindModeToggle } from './modeToggle';
 bindTitlebarEvents();
+bindModeToggle();
