02e1d7c6810c14c45a3c8cda9fc4bb477f2906ee
NicholasJohansan <nicholasjohansan@gmail.com>
2022-10-10 10:42:53 +0800

    Add pin button

--- src/functions/titlebar.js
+++ src/functions/titlebar.js
@@ -3,3 +3,10 @@ import { appWindow } from '@tauri-apps/api/window';
 
 $('#titlebar-minimize').on('click', () => appWindow.minimize());
 $('#titlebar-close').on('click', () => appWindow.close());
+
+let alwaysOnTop = false;
+$('#titlebar-pin').on('click', () => {
+  alwaysOnTop = !alwaysOnTop;
+  $('#titlebar-pin').toggleClass('active');
+  appWindow.setAlwaysOnTop(alwaysOnTop);
+});
